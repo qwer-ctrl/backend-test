7cddd8258591db3816ab14a3dad6965cdd2c043d
anki palviainen <anki.palviainen@gmail.com>
2022-11-26 21:07:47 +0200

    url

--- server.js
+++ server.js
@@ -5,21 +5,13 @@ import crypto from 'crypto'
 import bcrypt from 'bcrypt'
 import listEndpoints from 'express-list-endpoints'
 
-// import authRoute from "./routes/auth"
-// import exercisesRoute from "./routes/exercises"
-// import programsRoute from "./routes/programs"
-
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-final'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
 
